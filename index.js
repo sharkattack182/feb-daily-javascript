1b7d9b964b32e43725e09d7890147ed954f59962
Matthew Courtney <matthewdc182@gmail.com>
2021-02-02 18:57:38 -0500

    took about seven min

--- index.js
+++ index.js
@@ -20,9 +20,31 @@
 // multiplyNums("54, 75, 453, 0");
 // multiplyNums("10, -2");
 
-
 // Seven Boom
 // Create a function that takes in an array of numbers and returns "BOOM!" if the digit 7 appears.
-// Difficulty: Medium
+// Difficulty: Hard
 // Date: Feb 2 2021
-// Solve Time: 
+// Solve Time: 7 minutes
+
+function sevenBoom(array) {
+  let boom = false;
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    let elm = element.toString().split("");
+    for (let j = 0; j < elm.length; j++) {
+      const e = elm[j];
+      if (e == 7) {
+        boom = true;
+      }
+    }
+  }
+  if (boom === true) {
+    console.log("BOOM!");
+  } else {
+    console.log("No sevens in here");
+  }
+}
+
+sevenBoom([1, 2, 3, 4, 5, 6, 7]);
+sevenBoom([8, 6, 33, 100]);
+sevenBoom([2, 55, 60, 97, 86]);
