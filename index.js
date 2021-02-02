54cd921588a340d51fdb72cba748e5281eb82611
Matthew Courtney <matthewdc182@gmail.com>
2021-02-02 18:49:37 -0500

    added in daily challenge

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 // Multiplying numbers in a string
 // Given a string of numbers separated by a comma and space, return the product of the numbers.
+// Difficulty: Medium
 // Date: Feb 1 2021
 // Solve Time: 4 min
 
-function multiplyNums(string) {
-    var arr = string.split(", ");
-    let sum = 1;
-    for (let i = 0; i < arr.length; i++) {
-        const element = arr[i];
-        sum *= element;
-    }
-    console.log(sum)
-    return sum;
-}
+// function multiplyNums(string) {
+//     var arr = string.split(", ");
+//     let sum = 1;
+//     for (let i = 0; i < arr.length; i++) {
+//         const element = arr[i];
+//         sum *= element;
+//     }
+//     console.log(sum)
+//     return sum;
+// }
 
-multiplyNums("2, 3");
-multiplyNums("1, 2, 3, 4");
-multiplyNums("54, 75, 453, 0");
-multiplyNums("10, -2");
+// multiplyNums("2, 3");
+// multiplyNums("1, 2, 3, 4");
+// multiplyNums("54, 75, 453, 0");
+// multiplyNums("10, -2");
+
+
+// Seven Boom
+// Create a function that takes in an array of numbers and returns "BOOM!" if the digit 7 appears.
+// Difficulty: Medium
+// Date: Feb 2 2021
+// Solve Time: 
