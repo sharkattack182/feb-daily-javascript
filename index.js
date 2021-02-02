72ef66f8770c3936f7ec36b48e9cd52bc364278c
Matthew Courtney <matthewdc182@gmail.com>
2021-02-01 22:21:17 -0500

    done in four min

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 // Multiplying numbers in a string
 // Given a string of numbers separated by a comma and space, return the product of the numbers.
 // Date: Feb 1 2021
-// Solve Time: 
+// Solve Time: 4 min
 
+function multiplyNums(string) {
+    var arr = string.split(", ");
+    let sum = 1;
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        sum *= element;
+    }
+    console.log(sum)
+    return sum;
+}
+
+multiplyNums("2, 3");
+multiplyNums("1, 2, 3, 4");
+multiplyNums("54, 75, 453, 0");
+multiplyNums("10, -2");
