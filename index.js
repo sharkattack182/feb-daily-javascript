532636d1d94d7a7660958d708681bf6fd63f8a77
Matthew Courtney <matthewdc182@gmail.com>
2021-02-03 21:03:45 -0500

    added in daily problem

--- index.js
+++ index.js
@@ -26,25 +26,32 @@
 // Date: Feb 2 2021
 // Solve Time: 7 minutes
 
-function sevenBoom(array) {
-  let boom = false;
-  for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    let elm = element.toString().split("");
-    for (let j = 0; j < elm.length; j++) {
-      const e = elm[j];
-      if (e == 7) {
-        boom = true;
-      }
-    }
-  }
-  if (boom === true) {
-    console.log("BOOM!");
-  } else {
-    console.log("No sevens in here");
-  }
-}
+// function sevenBoom(array) {
+//   let boom = false;
+//   for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     let elm = element.toString().split("");
+//     for (let j = 0; j < elm.length; j++) {
+//       const e = elm[j];
+//       if (e == 7) {
+//         boom = true;
+//       }
+//     }
+//   }
+//   if (boom === true) {
+//     console.log("BOOM!");
+//   } else {
+//     console.log("No sevens in here");
+//   }
+// }
 
-sevenBoom([1, 2, 3, 4, 5, 6, 7]);
-sevenBoom([8, 6, 33, 100]);
-sevenBoom([2, 55, 60, 97, 86]);
+// sevenBoom([1, 2, 3, 4, 5, 6, 7]);
+// sevenBoom([8, 6, 33, 100]);
+// sevenBoom([2, 55, 60, 97, 86]);
+
+
+// Check if the brick fits
+// Write the function that takes three dimensions of a brick: height(a), width(b) and depth(c) and returns true if this brick can fit into a hole with the width(w) and height(h).
+// Difficulty: Hard
+// Date: Feb 3 2021
+// Solve Time: 
