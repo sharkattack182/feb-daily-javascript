f80e7ffe82f2df0df75cbdd5afd35fbe41fd8f7b
Matthew Courtney <matthewdc182@gmail.com>
2021-02-11 21:19:34 -0500

    im sure there was a better way to go about this. I tried my best to trim the fat from the code I completely disregarded the outside of the goal and the lower portion of the over all arrray. I only targeted the options that could possibly be true rather than searching each spavce individually

--- index.js
+++ index.js
@@ -49,7 +49,6 @@
 // sevenBoom([8, 6, 33, 100]);
 // sevenBoom([2, 55, 60, 97, 86]);
 
-
 // Check if the brick fits
 // Write the function that takes three dimensions of a brick: height(a), width(b) and depth(c) and returns true if this brick can fit into a hole with the width(w) and height(h).
 // Difficulty: Hard
@@ -76,7 +75,6 @@
 //         fits ++;
 //     }
 
-
 //     if(fits >= 2) {
 //         console.log(true)
 //     } else {
@@ -84,12 +82,9 @@
 //     }
 // }
 
-
 // doesBrickFit(1, 1, 1, 1, 1);
 // doesBrickFit(1, 2, 1, 1, 1);
 // doesBrickFit(1, 2, 2, 1, 1);
-
-
 
 // Reverse the odd length words
 // Given a string, reverse all the words which have odd length. The even length words are not changed.
@@ -109,12 +104,12 @@
 //             let newArr = []
 //             for (let j = normArr.length -1; j >= 0; j--) {
 //                 newArr.push(normArr[j]);
-                
+
 //             }
 //             var rev = newArr.join("");
 //             returnArray.push(rev)
 //         }
-        
+
 //     }
 //     console.log(returnArray.join(" "))
 // }
@@ -123,13 +118,11 @@
 // reverseOdd("One two three four");
 // reverseOdd("Make sure uoy only esrever sdrow of ddo length");
 
-
 // Temperature Converter
 // Create a function that converts Celcius to Fahrenheit and vice versa.
 // Difficulty: Hard
 // Date: Feb 5 2021
 // Solve Time: 16 min
-
 
 // function convert(string) {
 //     if(string.includes("F")) {
@@ -139,24 +132,21 @@
 //     } else if(string.includes("C")) {
 //      var degreesC = parseFloat(string);
 //      var tempFaren = (degreesC * 9/5) + 32;
-//      console.log(Math.round(tempFaren) + " °F")   
+//      console.log(Math.round(tempFaren) + " °F")
 //     } else {
 //         console.log("Not a temp")
 //     }
-// } 
-
+// }
 
 // convert("35°C");
 // convert("19°F");
 // convert("33");
-
 
 // Find First Character that Repeats
 // Create a function that takes a string and returns the first character that repeats. If there is no repeat of a character, return "-1".
 // Difficulty: Hard
 // Date: Feb 6 2021
 // Solve Time: 6 min
-
 
 // function firstRepeat(string) {
 //     var newString ="";
@@ -183,12 +173,11 @@
 // firstRepeat("Balrog");
 // firstRepeat("Isildur");
 
-
 // Length of a nested array
 // Write a function that returns the total number of non-nested items in a nested array.
 // Difficulty: Hard
 // Date: Feb 7 2021
-// Solve Time: 
+// Solve Time:
 
 // function getLength(array) {
 //     console.log(array.flat(Infinity).length)
@@ -198,7 +187,6 @@
 // getLength([1, [2, [3, 4]]]);
 // getLength([1, [2, [3, [4, [5, 6]]]]]);
 // getLength([1, [2], 1, [2], 1]);
-
 
 // Oddish vs Evenish
 // Create a function that determines whether a number is Oddish or Evenish. A number is Oddish if the sum of all of its digits is odd, and a number is Evenish if the sum of all of its digits is even. If a number is Oddish, return "Oddish". Otherwise, return "Evenish".
@@ -224,7 +212,6 @@
 // oddishOrEvenish(43);
 // oddishOrEvenish(373);
 // oddishOrEvenish(4433);
-
 
 // Changing a string into camelCase
 // Create a function that takes a string and returns it back in camelCase.
@@ -254,7 +241,6 @@
 // camelCasing("snake_case");
 // camelCasing("low high_HIGH");
 
-
 // Combinations
 // Create a function that takes a variable number of arguments, each argument representing the number of items in a group, and returns the number of permutations (combinations) of items that you could get by taking one item from each group.
 // Difficulty: Hard
@@ -276,11 +262,57 @@
 // combinations(3, 7, 4);
 // combinations(2, 3, 4, 5);
 
-
-
 // Scoring a field goal
 // In (American) Football, a team can score if they manage to kick a ball through the goal (i.e. above the crossbar and between the uprights). Create a function that returns true if the ball 0 goes through the goal. You will be given an array of arrays.
 // Difficulty: Hard
 // Date: Feb 11 2021
-// Solve Time: 
+// Solve Time: 18 min
 
+function isGoalScored(...arrays) {
+  var mainArr = arrays[0];
+  var fieldGoal = false;
+  for (let i = 0; i < 3; i++) {
+    const element = mainArr[i];
+    for (let j = 0; j < element.length; j++) {
+      const smallArr = element[j];
+      if (smallArr.includes(0)) {
+        for (let r = 3; r < 7; r++) {
+          const element = smallArr[r];
+          if (element === "0") {
+            fieldGoal = true;
+          }
+        }
+      }
+    }
+  }
+  console.log(fieldGoal);
+}
+
+isGoalScored([
+  ["  #     #  "],
+  ["  #  0  #  "],
+  ["  #     #  "],
+  ["  #######  "],
+  ["     #     "],
+  ["     #     "],
+  ["     #     "],
+]);
+isGoalScored([
+  ["  #0    #  "],
+  ["  #     #  "],
+  ["  #     #  "],
+  ["  #######  "],
+  ["     #     "],
+  ["     #     "],
+  ["     #     "],
+]);
+
+isGoalScored([
+  ["  #     #  "],
+  ["  #     #  "],
+  ["  #     # 0"],
+  ["  #######  "],
+  ["     #     "],
+  ["     #     "],
+  ["     #     "],
+]);
