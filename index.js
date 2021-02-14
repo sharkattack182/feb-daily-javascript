22dea19ef91378904c49280d6f73e4e7ef3c583b
Matthew Courtney <matthewdc182@gmail.com>
2021-02-13 19:56:46 -0500

    took about 20 min was making dinner while i did it

--- index.js
+++ index.js
@@ -346,4 +346,34 @@
 // Create a function that takes a value as an argument and returns the type of this value. You should get the real type of a value (JavaScript typeof doesn't return the real object type of values and you need to fix that).
 // Difficulty: Hard
 // Date: Feb 13 2021
-// Solve Time: 
+// Solve Time: 20 min
+
+
+function realType(data) {
+  var dataType;
+  if(typeof data !== "object") {
+    dataType = typeof data
+  }
+  else if(Object.prototype.toString.call(data) === '[object Array]') {
+  dataType = "array"
+  } else {
+    dataType = null;
+  }
+
+  console.log(dataType)
+}
+
+realType(1) ;
+// "number"
+
+realType("a") ;
+// "string"
+
+realType(true) ;
+// "boolean"
+
+realType([]) ;
+// "array"
+
+realType(null);
+// "null"
